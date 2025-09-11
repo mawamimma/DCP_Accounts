2a861e88f4f505f0373e2f77b374b8777e029b39
WALTON <WALTON@MIMMA>
2025-09-11 23:58:47 +0600

    mp3

---
+++ DCP_Accounts_Api/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using DCP_Accounts_Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DCP_Accounts_Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Expend> Expends { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Expend>(entity =>
+            {
+                entity.ToTable("tblExpend");
+                entity.HasKey(e => e.ExpenditurID);
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ DCP_Accounts_Api/Models/Expend.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DCP_Accounts_Api.Models
+{
+    [Table("tblExpend")]
+    public class Expend
+    {
+        [Key]
+        public int ExpenditurID { get; set; }
+
+        [StringLength(4)]
+        public string? ExpSubCode { get; set; }
+
+        public DateTime? ExpDate { get; set; }
+
+        [StringLength(5)]
+        public string? UID { get; set; }
+
+        public DateTime? EntryDate { get; set; }
+    }
+}
