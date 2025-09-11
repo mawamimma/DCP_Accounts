113e5014a5afe6d0a8ea5e901c31df1f04056569
Nasir <Nasir@NASIR_DCP>
2025-09-12 04:21:45 +0600

    nk 4th push

--- DCP_Accounts_Api/Program.cs
+++ DCP_Accounts_Api/Program.cs
@@ -13,27 +13,27 @@ builder.Services.AddControllers().AddNewtonsoftJson();
 
 // Swagger
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "DCP Accounts API",
-        Version = "v1",
-        Description = "API for tblExpend with 5 fields only"
-    });
-});
+//builder.Services.AddSwaggerGen(c =>
+//{
+//    c.SwaggerDoc("v1", new OpenApiInfo
+//    {
+//        Title = "DCP Accounts API",
+//        Version = "v1",
+//        Description = "API for tblExpend with 5 fields only"
+//    });
+//});
 
 var app = builder.Build();
 
 // Swagger UI
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "DCP Accounts API v1");
-    });
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI(c =>
+//    {
+//        c.SwaggerEndpoint("/swagger/v1/swagger.json", "DCP Accounts API v1");
+//    });
+//}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
